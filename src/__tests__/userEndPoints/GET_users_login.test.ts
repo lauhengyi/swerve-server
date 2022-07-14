fbe41043171d5958fa0026165ec491abb058d908
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-07-14 21:04:14 +0800

    Added login functionality for users

---
+++ src/__tests__/userEndPoints/GET_users_login.test.ts
@@ -0,0 +1,217 @@
+import app from '../../app';
+import request from 'supertest';
+import testDBSetup from '../../testUtils/testDBSetup';
+
+testDBSetup();
+
+describe('GET /users/login', () => {
+  describe('When a valid email and password is provided', () => {
+    it('Should have a status code of 200', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      expect(response.statusCode).toBe(200);
+    });
+    it('Should respond with status of success, and the jwt token of the logged in user', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      const expectedMessage = {
+        status: 'success',
+        token: expect.any(String)
+      };
+      expect(response.body).toEqual(expectedMessage);
+    });
+  });
+  describe('When the email is not provided', () => {
+    it('Should have a status code 400', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      expect(response.statusCode).toBe(400);
+    });
+    it('Should respond with a status of fail and a message of "Please provide an email and password."', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      const expectedMessage = {
+        status: 'fail',
+        message: 'Please provide an email and password.'
+      };
+      expect(response.body).toEqual(expectedMessage);
+    });
+  });
+  describe('When the password is not provided', () => {
+    it('Should have a status code 400', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      expect(response.statusCode).toBe(400);
+    });
+    it('Should respond with a status of fail and a message of "Please provide an email and password."', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      const expectedMessage = {
+        status: 'fail',
+        message: 'Please provide an email and password.'
+      };
+      expect(response.body).toEqual(expectedMessage);
+    });
+  });
+  describe('When the email is incorrect', () => {
+    it('Should have a status code of 401', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dremill@gmail.com',
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('Should have a status of fail with a message of "Email or password is incorrect"', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dremill@gmail.com',
+        password: 'heng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      const expectedMessage = {
+        status: 'fail',
+        message: 'Email or password is incorrect.'
+      };
+      expect(response.body).toEqual(expectedMessage);
+    });
+  });
+
+  describe('When the password is incorrect', () => {
+    it('Should have a status code of 401', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com',
+        password: 'hng1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('Should have a status of fail with a message of "Email or password is incorrect"', async () => {
+      const user = {
+        username: 'dreadmill_gratis',
+        email: 'dreadmill@gmail.com',
+        password: 'heng1230@sjfl.',
+        passwordConfirm: 'heng1230@sjfl.',
+        accountType: 'regular'
+      };
+      await request(app).post('/api/v1/users/signup').send(user);
+      const body = {
+        email: 'dreadmill@gmail.com',
+        password: 'heg1230@sjfl.'
+      };
+
+      const response = await request(app).get('/api/v1/users/login').send(body);
+
+      const expectedMessage = {
+        status: 'fail',
+        message: 'Email or password is incorrect.'
+      };
+      expect(response.body).toEqual(expectedMessage);
+    });
+  });
+});
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,7 +6,7 @@ import AppError from '../utils/AppError';
 
 const userDatabase = new Database(User);
 
-const signup = catchAsync(async (req, res, next) => {
+const signUp = catchAsync(async (req, res, next) => {
   // Check for the JWT details in environment variables
   if (!process.env.JWT_SECRET) {
     return next(new AppError('JWT_SECRET is not defined', 500));
@@ -36,4 +36,34 @@ const signup = catchAsync(async (req, res, next) => {
   });
 });
 
-export default { signup };
+const logIn = catchAsync(async (req, res, next) => {
+  // Check for the JWT details in environment variables
+  if (!process.env.JWT_SECRET) {
+    return next(new AppError('JWT_SECRET is not defined', 500));
+  }
+  if (!process.env.JWT_EXPIRES_IN) {
+    return next(new AppError('JWT_EXPIRES_IN is not defined', 500));
+  }
+
+  // Check the request for email and password
+  if (!req.body.email || !req.body.password) {
+    return next(new AppError('Please provide an email and password.', 400));
+  }
+
+  const doc = await userDatabase.findOne({ email: req.body.email });
+
+  if (!doc || !(await doc.comparePassword(req.body.password))) {
+    return next(new AppError('Email or password is incorrect.', 401));
+  }
+
+  const token = jwt.sign({ id: doc._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+});
+
+export default { signUp, logIn };
