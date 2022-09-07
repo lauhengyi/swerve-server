45299b39e33f3aa52354fd0233544114f5ee5263
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-09-07 22:46:16 +0800

    Made request body limit to 10kb

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(
 
 // Parses incoming requests with JSON payloads.
 // The new body object will be empty if the request body is not JSON.
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 // Routes
 const apiString = '/api/v1';
